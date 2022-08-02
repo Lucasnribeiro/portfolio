cee506c5d0ef76b3a3b63e10eba94a628f524a0c
Lucas <lucasmnribeiro@gmail.com>
2022-08-02 14:56:24 -0300

    mantine ui and google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
+import { MantineProvider } from '@mantine/core'
 import '../styles/globals.css'
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+      
+      <Head>
+        <title>Lucas Ribeiro</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider withCSSVariables
+                      withNormalizeCSS
+                      theme={{colorScheme: 'light'}}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  ) 
+  
 }
 
 export default MyApp
